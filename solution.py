f5e8dd5c4ab1bab81ea8bd139da4a32d1e14f13d
Pavel Gogolev <122290372+700xp@users.noreply.github.com>
2023-04-05 17:08:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 689327667 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return (10 * len(x) / sum(x))
+    return (4 + x.mean())/10
